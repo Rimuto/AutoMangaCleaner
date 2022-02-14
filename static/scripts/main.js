715dc809f45644e916d0e43d30980369569e435e
Rimuto <tamir_98@mail.ru>
2022-02-14 11:20:11 +1000

    added fabricjs canvas

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,7 +1,309 @@
+$(function() {
+
 var id = 0;
 var selected_div;
 
-$( document ).ready(function() {
+const STEP = 1;
+var Direction = {
+    LEFT: 0,
+    UP: 1,
+    RIGHT: 2,
+    DOWN: 3
+};
+
+fabric.util.addListener(document.body, 'keydown', function (options) {
+    if (options.repeat) {
+        return;
+    }
+    var key = options.which || options.keyCode; // key detection
+    if (key === 37) { // handle Left key
+        options.preventDefault();
+        moveSelected(Direction.LEFT);
+    } else if (key === 38) { // handle Up key
+        options.preventDefault();
+        moveSelected(Direction.UP);
+    } else if (key === 39) { // handle Right key
+        options.preventDefault();
+        moveSelected(Direction.RIGHT);
+    } else if (key === 40) { // handle Down key
+        options.preventDefault();
+        moveSelected(Direction.DOWN);
+    }
+});
+
+function moveSelected(direction) {
+    var activeObject = canvas.getActiveObject();
+    var activeGroup = canvas.getActiveGroup;
+
+    if (activeObject) {
+        switch (direction) {
+        case Direction.LEFT:
+            activeObject.left = (activeObject.left - STEP);
+            break;
+        case Direction.UP:
+            activeObject.top = (activeObject.top - STEP);
+            break;
+        case Direction.RIGHT:
+            activeObject.left = (activeObject.left + STEP);
+            break;
+        case Direction.DOWN:
+            activeObject.top = (activeObject.top + STEP);
+            break;
+        }
+        activeObject.setCoords();
+        canvas.renderAll();
+        console.log('selected objects was moved');
+    } else if (activeGroup) {
+        switch (direction) {
+        case Direction.LEFT:
+            activeGroup.left = (activeGroup.left - STEP);
+            break;
+        case Direction.UP:
+            activeGroup.top = (activeGroup.top - STEP);
+            break;
+        case Direction.RIGHT:
+            activeGroup.left = (activeGroup.left + STEP);
+            break;
+        case Direction.DOWN:
+            activeGroup.top = (activeGroup.top + STEP);
+            break;
+        }
+        activeGroup.setCoords();
+        canvas.renderAll();
+        console.log('selected group was moved');
+    } else {
+        console.log('no object selected');
+    }
+
+}
+
+var canvas = new fabric.Canvas("canvas");
+canvas.on('text:selection:changed', onSelectionChanged);
+canvas.on('mouse:down', function(opt) {
+  var evt = opt.e;
+  if (evt.altKey === true) {
+    this.isDragging = true;
+    this.selection = false;
+    this.lastPosX = evt.clientX;
+    this.lastPosY = evt.clientY;
+  }
+});
+canvas.on('mouse:move', function(opt) {
+  if (this.isDragging) {
+    var e = opt.e;
+    var vpt = this.viewportTransform;
+    vpt[4] += e.clientX - this.lastPosX;
+    vpt[5] += e.clientY - this.lastPosY;
+    this.requestRenderAll();
+    this.lastPosX = e.clientX;
+    this.lastPosY = e.clientY;
+  }
+});
+canvas.on('mouse:up', function(opt) {
+  // on mouse up we want to recalculate new interaction
+  // for all objects, so we call setViewportTransform
+  this.setViewportTransform(this.viewportTransform);
+  this.isDragging = false;
+  this.selection = true;
+});
+canvas.on('mouse:wheel', function(opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 0.01) zoom = 0.01;
+  canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+});
+
+
+
+
+
+$( "#delete" ).click(function() {
+    let activeObject = canvas.getActiveObjects();
+    if (activeObject.length > 0) {
+        let objectsInGroup = activeObject;
+        canvas.discardActiveObject();
+        objectsInGroup.forEach(function(object) {
+        canvas.remove(object);
+      });
+    }
+});
+
+canvas.on('selection:updated', function(options) {
+    let activeObject = canvas.getActiveObjects();
+    $('#angle').val(activeObject[0].angle);
+});
+canvas.on('selection:created', function(options) {
+    let activeObject = canvas.getActiveObjects();
+    $('#angle').val(activeObject[0].angle);
+});
+canvas.on('object:rotating', function(options) {
+    let activeObject = canvas.getActiveObjects();
+    $('#angle').val(activeObject[0].angle);
+
+    //console.log($('#text-font-size').val());
+});
+
+$( "#add" ).click(function() {
+    var text = new fabric.Textbox('A Computer Science Portal',
+    {
+        cornerSize: 10,
+        lockScalingY: true,
+        lockUniScaling: true,
+        width: 300,
+        left: 200,
+        top: 50,
+        fontSize: 21,
+        originX: "center",
+        originY: "center"
+    });
+    canvas.add(text);
+    canvas.renderAll();
+});
+
+$("#angle").on("change", function() {
+  if (canvas.getActiveObjects().length > 0){
+    canvas.getActiveObject().set("angle", $("#angle").val());
+    canvas.renderAll();
+    }
+});
+
+$("#font-family").on("change", function() {
+  var obj = canvas.getActiveObject();
+  if (obj) {
+    setStyle(obj, 'fontFamily', $("#font-family").val());
+    canvas.renderAll();
+  }
+});
+
+$("#text-color").on("input", function() {
+  var obj = canvas.getActiveObject();
+  if (obj) {
+    setStyle(obj, 'fill', $("#text-color").val());
+    canvas.renderAll();
+  }
+});
+
+
+$("#text-font-size").on("change", function() {
+  var obj = canvas.getActiveObject();
+  if (obj) {
+    setStyle(obj, 'fontSize', $("#text-font-size").val());
+    canvas.renderAll();
+  }
+});
+
+
+function setStyle(object, styleName, value) {
+  if (object.setSelectionStyles && object.isEditing) {
+    var style = {};
+    style[styleName] = value;
+    object.setSelectionStyles(style);
+  } else {
+    object[styleName] = value;
+
+  }
+}
+
+function onSelectionChanged() {
+  var obj = canvas.getActiveObject();
+  if (obj.selectionStart > -1) {
+    $('#fontSize').val(getStyle(obj, 'fontSize'));
+  }
+}
+
+function getStyle(object, styleName) {
+  return (object.getStyleAtPosition && object.isEditing) ?
+    object.getStyleAtPosition(object.selectionStart)[styleName] : object[styleName];
+}
+
+
+
+
+
+var createImage = function(src, title) {
+  var img   = new Image();
+  img.src   = src;
+  img.alt   = title;
+  img.title = title;
+  return img;
+};
+
+// array of images
+var images = [];
+var current = 0;
+var max = 0;
+
+$('#next').on({
+    'click': function(){
+        if (current + 1 > max){
+            current = 0
+        }
+        else{
+            current = current + 1;}
+        $('#myimage').attr('src', images[current].src);
+    }
+});
+
+$('#prev').on({
+    'click': function(){
+        if (current - 1 < 0){
+            current = max
+        } else {
+            current = current - 1;}
+        $('#myimage').attr('src', images[current].src);
+    }
+});
+
+$('#upload-file-btn').click(function() {
+    var form_data = new FormData($('#upload-file')[0]);
+    $.ajax({
+        type: 'POST',
+        url: '/uploadajax',
+        data: form_data,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function(data) {
+            images = [];
+            for(var k in data) {
+                console.log(k, data[k]);
+                images.push(createImage('data:image/png;base64,' + data[k].img, k));
+                max = k;
+            }
+             fabric.Image.fromURL(images[0].src, function(img) {
+                 canvas.setDimensions({width:img.width*2, height:img.height*2});
+                 // add background image
+                 canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
+              });
+        },
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+/*$( document ).ready(function() {
     const element = document.getElementById('mainContainer')
     const panzoom = Panzoom(element, {
         bound:'outer',
@@ -215,76 +517,10 @@ let saveImageWithText = () => {
         a.click();
     }
 }
-
+*/
 
 
 
 // Image factory
-var createImage = function(src, title) {
-  var img   = new Image();
-  img.src   = src;
-  img.alt   = title;
-  img.title = title;
-  return img;
-};
-
-// array of images
-var images = [];
-var current = 0;
-var max = 0;
-
-$(function(){
-$('#next').on({
-    'click': function(){
-        if (current + 1 > max){
-            current = 0
-        }
-        else{
-            current = current + 1;}
-        $('#myimage').attr('src', images[current].src);
-    }
-});
-});
 
 
-$(function(){
-$('#prev').on({
-    'click': function(){
-        if (current - 1 < 0){
-            current = max
-        } else {
-            current = current - 1;}
-        $('#myimage').attr('src', images[current].src);
-    }
-});
-});
-
-
-$(function(){
-$('#upload-file-btn').click(function() {
-    var form_data = new FormData($('#upload-file')[0]);
-    $.ajax({
-        type: 'POST',
-        url: '/uploadajax',
-        data: form_data,
-        contentType: false,
-        cache: false,
-        processData: false,
-        success: function(data) {
-            images = [];
-            for(var k in data) {
-                console.log(k, data[k]);
-                images.push(createImage('data:image/png;base64,' + data[k].img, k));
-                max = k;
-                //var img = $('<img id="image_id">');
-                //img.attr('src', 'data:image/png;base64,' + data[k].img);
-                //img.appendTo('#imgContainer');
-            }
-            var img = $('<img id="image_id">');
-            img.attr('src', images[0].src);
-            img.appendTo('#imgContainer');
-            //console.log(data);
-        },
-    });
-});
-});
